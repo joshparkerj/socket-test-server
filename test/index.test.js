a809c040b0c632ff1dba9f5190d9e0ed19aa8409
Josh Parker <joshBBparker@gmail.com>
2022-03-10 15:18:38 -0700

    add second test

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai');
+
 const { io: Client } = require('socket.io-client');
 const getServerStarter = require('../index');
 
@@ -18,8 +20,30 @@ describe('first test', () => {
   });
 
   it('should respond', (done) => {
-    clientSocket.emit('addPerson', { name: 'test', age: 0 });
     clientSocket.emit('getPeople');
-    clientSocket.on('receiveAllPeople', () => done());
+    clientSocket.on('receiveAllPeople', () => {
+      clientSocket.off('receiveAllPeople');
+      done();
+    });
+  });
+
+  it('should accept added people', (done) => {
+    const people = [
+      { name: 'test 0', age: 0 },
+      { name: 'test 1', age: 1 },
+      { name: 'test 2', age: 2 },
+      { name: 'test 3', age: 3 },
+      { name: 'test 4', age: 4 },
+    ];
+
+    people.forEach((person) => clientSocket.emit('addPerson', person));
+
+    clientSocket.emit('getPeople');
+    clientSocket.on('receiveAllPeople',
+      (data) => {
+        clientSocket.off('receiveAllPeople');
+        assert.deepEqual(data, people);
+        done();
+      });
   });
 });
