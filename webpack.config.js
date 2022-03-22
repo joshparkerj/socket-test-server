a0df8200c7f5bfce6723595a6af37b99a9f29e34
Josh Parker <joshBBparker@gmail.com>
2022-03-21 19:41:04 -0600

    switch loader from babel to swc

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,25 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        loader: 'swc-loader',
         options: {
-          presets: [
-            '@babel/preset-react',
-          ],
+          /* Options ripped off from https://gist.github.com/bo01ean/532c33e20c0c79c5b6d1541fa3c9c4e9 */
+          jsc: {
+            parser: {
+              syntax: 'ecmascript',
+              jsx: true,
+            },
+            transform: {
+              react: {
+                pragma: 'React.createElement',
+                pragmaFrag: 'React.fragment',
+                throwIfNamespace: true,
+                development: true,
+                useBuiltins: false,
+              },
+            },
+          },
         },
       },
     ],
